c2882feb643f073244556957b79e22af62bfcc87
NextGenerationTeam <nextgenerationteam@NGT.local>
2019-10-25 20:07:19 +0300

    app

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList';
+
+export default function App() {
+  return (
+    <EventList />
+  );
+}
+
---
+++ EventCard.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet,} from 'react-native';
+import PropTypes from 'prop-types';
+import {formatDate, getCountdownParts,} from './api';
+
+
+export default function EventCard({ event }) {
+    
+}
